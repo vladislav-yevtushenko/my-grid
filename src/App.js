4bda6b5c688b2b9f3d136a9b6901817f84d5954e
Vladislav Yevtushenko <vladislav_yevtushenko@epam.com>
2022-07-10 16:30:22 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import 'handsontable/dist/handsontable.full.css';
+import { HotTable } from '@handsontable/react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.data = [
+            ["", "Ford", "Volvo", "Toyota", "Honda"],
+            ["2016", 10, 11, 12, 13],
+            ["2017", 20, 11, 14, 13],
+            ["2018", 30, 15, 12, 13]
+        ];
+    }
+
+    render() {
+        return ( <
+            div id = "hot-app" >
+            <
+            HotTable data = { this.data }
+            colHeaders = { true }
+            rowHeaders = { true }
+            width = "600"
+            height = "300"
+            stretchH = "all" / >
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
